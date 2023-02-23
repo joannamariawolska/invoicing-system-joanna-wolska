bcb6a1d63d9aeb81beaab0371d9af65b19b3f172
Joanna Wolska <joanna.maria.wolska@gmail.com>
2023-02-23 12:04:59 +0100

    updated to show API usage example

--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,13 +1,63 @@
 package pl.futurecollars.invoicing.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import com.google.common.base.MoreObjects;
+import java.util.Objects;
 
-@Data
-@AllArgsConstructor
 public class Company {
-  private int id;
+
   private String taxIdentificationNumber;
   private String address;
+  private String name;
 
+  public Company(String taxIdentificationNumber, String address, String name) {
+    this.taxIdentificationNumber = taxIdentificationNumber;
+    this.address = address;
+    this.name = name;
+  }
+
+  public String getTaxIdentificationNumber() {
+    return taxIdentificationNumber;
+  }
+  public void setTaxIdentificationNumber(String taxIdentificationNumber) {
+    this.taxIdentificationNumber = taxIdentificationNumber;
+  }
+  public String getAddress() {
+    return address;
+  }
+  public void setAddress(String address) {
+    this.address = address;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Company company = (Company) o;
+    return Objects.equals(taxIdentificationNumber, company.taxIdentificationNumber)
+        && Objects.equals(address, company.address)
+        && Objects.equals(name, company.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(taxIdentificationNumber, address, name);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("taxIdentificationNumber", taxIdentificationNumber)
+        .add("address", address)
+        .add("name", name)
+        .toString();
+  }
 }
--- app/src/main/java/pl/futurecollars/invoicing/model/Invoice.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Invoice.java
@@ -1,18 +1,76 @@
 package pl.futurecollars.invoicing.model;
-
-import java.time.LocalDateTime;
+import com.google.common.base.MoreObjects;
+import java.time.LocalDate;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
+import java.util.Objects;
 public class Invoice {
-
-  private long id;
-  private LocalDateTime date;
+  private int id;
+  private LocalDate date;
   private Company buyer;
   private Company seller;
   private List<InvoiceEntry> entries;
 
+  public Invoice(LocalDate date, Company buyer, Company seller, List<InvoiceEntry> entries) {
+    this.date = date;
+    this.buyer = buyer;
+    this.seller = seller;
+    this.entries = entries;
+  }
+
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public LocalDate getDate() {
+    return date;
+  }
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+  public Company getBuyer() {
+    return buyer;
+  }
+  public void setBuyer(Company buyer) {
+    this.buyer = buyer;
+  }
+  public Company getSeller() {
+    return seller;
+  }
+  public void setSeller(Company seller) {
+    this.seller = seller;
+  }
+  public List<InvoiceEntry> getEntries() {
+    return entries;
+  }
+  public void setEntries(List<InvoiceEntry> entries) {
+    this.entries = entries;
+  }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Invoice invoice = (Invoice) o;
+    return id == invoice.id && Objects.equals(date, invoice.date) && Objects.equals(buyer, invoice.buyer)
+        && Objects.equals(seller, invoice.seller) && Objects.equals(entries, invoice.entries);
+  }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, date, buyer, seller, entries);
+  }
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("id", id)
+        .add("date", date)
+        .add("buyer", buyer)
+        .add("seller", seller)
+        .add("entries", entries)
+        .toString();
+  }
 }
