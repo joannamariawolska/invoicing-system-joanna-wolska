3318384888a6f168ae4b8a74fbc4b93531341467
Joanna Wolska <joanna.maria.wolska@gmail.com>
2023-03-19 10:45:59 +0100

    corrected checkstyle

--- app/src/main/java/pl/futurecollars/invoicing/db/file/FileBasedDatabase.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/file/FileBasedDatabase.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Repository;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.utils.FilesService;
