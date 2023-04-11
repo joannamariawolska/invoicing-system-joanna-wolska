d1b238b22e3ebf30ec928b8e422ebd3358917b61
Joanna Wolska <64964299+joannamariawolska@users.noreply.github.com>
2023-04-11 16:53:17 +0200

    Task9 (#15)
    * TaxCalculatorService and TaxCalculatorController
    
    * corrected checkstyle

--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,25 +1,22 @@
 package pl.futurecollars.invoicing.model;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Company {
 
   @ApiModelProperty(value = "Tax identification number", required = true, example = "552-168-66-00")
   private String taxIdentificationNumber;
-
-  @ApiModelProperty(value = "Company address", required = true, example = "ul. Gdańska 14, 85-100 Bydgoszcz")
+  @ApiModelProperty(value = "Company address", required = true, example = "ul. Mazowiecka 134, 32-525 Radzionkow")
   private String address;
-
   @ApiModelProperty(value = "Company name", required = true, example = "Invoice House Ltd.")
   private String name;
 
-  public Company(String taxIdentificationNumber, String address, String name) {
-    this.taxIdentificationNumber = taxIdentificationNumber;
-    this.address = address;
-    this.name = name;
-  }
 }
